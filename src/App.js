7db1a3190948549f7a2aec02228105f0a59da0ac
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2023-02-04 12:11:39 +0530

    Commit - 6 vh and vw for mobile screens px locked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, Divider, Stack, Typography } from '@mui/material'
+import { Box, Divider, Stack } from '@mui/material'
 import AppsBar from './Components/AppsBar';
 import SideBar from './Components/SideBar';
 import Content from './Components/Content';
@@ -21,10 +21,19 @@ function App() {
             </Box>
           </Stack>
         </Box>
-        <Box maxWidth='100vw' sx={{ display: { xs: 'flex', md: 'none' } }}>
-          <Box display='flex' justifyContent='center' alignItems='center' >
-            <Typography component='h2' variant='h2'>🚧⚠️This App is not optimized for mobile view, Please switch to a desktop view.⚠️🚧</Typography>
-          </Box>
+        <Box height='768px' width='1366px' sx={{ display: { xs: 'block', lg: 'none' } }}>
+          <Stack direction='row' bgcolor='#ffffff' minHeight='100%'  >
+            <Box flex={2} px={0}>
+              <SideBar />
+            </Box>
+            <Box flex={10} px={0} bgcolor='#f0f6f6' minWidth='82%' minHeight='100%' justifyContent='center' alignItems='center'>
+              <Stack direction='column' maxWidth='90%' mx='auto' minHeight='100%' >
+                <AppsBar />
+                <Divider />
+                <Content />
+              </Stack>
+            </Box>
+          </Stack>
         </Box>
       </Box>
     </>
